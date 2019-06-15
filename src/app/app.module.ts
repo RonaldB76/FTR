29adb631419e0932b5c99c20e5b1c3a1650eef73
RonaldB76 <info@ronaldbischoff.de>
2019-06-15 20:23:09 +0200

    basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,49 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './basics/navigation/navigation.component';
+import { TopComponent } from './basics/top/top.component';
+import { FooterComponent } from './basics/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './basics/toolbar/toolbar.component';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatGridListModule
+ } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavigationComponent,
+      TopComponent,
+      FooterComponent,
+      HomeComponent,
+      ToolbarComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule,
+      MatCardModule,
+      MatGridListModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
